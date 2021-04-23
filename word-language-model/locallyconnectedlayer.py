2c00611032f967b17b91a7e0622b4e13ef880d16
dsingh33 <dsingh33@jhu.edu>
2021-04-23 19:30:52 -0400

    locally connected layer skeleton code

---
+++ word-language-model/locallyconnectedlayer.py
@@ -0,0 +1,36 @@
+import argparse
+import time
+import math
+import torch
+import torch.nn as nn
+import torch.onnx
+from torch.autograd import Variable
+from progress.bar import Bar
+import data
+import model
+import sys
+import numpy as np
+import random
+
+class LocallyConnected2d(nn.Module):
+    def __init__(self, in_channels, out_channels, output_size, kernel_size, stride, bias=False):
+        super(LocallyConnected2d, self).__init__()
+        output_size = _pair(output_size)
+        self.weight = nn.Parameter(
+            torch.randn(1, out_channels, in_channels, output_size[0], output_size[1], kernel_size**2)
+        )
+        if bias:
+            self.bias = nn.Parameter(
+                torch.randn(1, out_channels, output_size[0], output_size[1])
+            )
+        else:
+            self.register_parameter('bias', None)
+        self.kernel_size = _pair(kernel_size)
+        self.stride = _pair(stride)
+        
+    def forward(self, x):
+        _, c, h, w = x.size()
+        kh, kw = self.kernel_size
+        dh, dw = self.stride
+        x = x.unfold(2, kh, dh).unfold(3, kw, dw)
+        x = x.contiguous().view(*x.size()[:-2], -1)
