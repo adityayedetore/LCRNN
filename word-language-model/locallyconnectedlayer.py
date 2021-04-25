7de47b363e58d87486f248bda539ae5be0cdbc46
eweeding <70592741+eweeding@users.noreply.github.com>
2021-04-25 17:32:51 -0400

    Revert "Revert "update locally connected layers""
    This reverts commit de8dc07b19dcfc9d851db9ff1be36a2d2c38783f.

--- word-language-model/locallyconnectedlayer.py
+++ word-language-model/locallyconnectedlayer.py
@@ -12,6 +12,8 @@ import sys
 import numpy as np
 import random
 
+# idea credit: https://discuss.pytorch.org/t/locally-connected-layers/26979
+
 class LocallyConnected2d(nn.Module):
     def __init__(self, in_channels, out_channels, output_size, kernel_size, stride, bias=False):
         super(LocallyConnected2d, self).__init__()
